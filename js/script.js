2dc7c8a8a286274269a4b602befcd033109ffd66
CristiamFlores <103546758+CristiamFlores@users.noreply.github.com>
2023-03-13 17:48:07 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -47,7 +47,7 @@ class Memorama {
             const imgLabel = card.children[1].children[0];
 
             card.dataset.image = image;
-            imgLabel.src = `./images/${image}.jpg`;
+            imgLabel.src = `https://randomfox.ca/images/${image}.jpg`;
 
         }
 
@@ -173,3 +173,8 @@ document.addEventListener("DOMContentLoaded", () => {
     new Memorama();
 
 });
+function redireccion(){
+    location.href = "index.html"
+
+}
+
